077fdc10052486c6a4d689513058dc3be98affb6
npolyak <nick.polyak@verizon.net>
2021-07-19 15:29:27 -0400

    Added a project for primitives

--- NP.Demos.BuiltInControls/MainWindow.axaml.cs
+++ NP.Demos.BuiltInControls/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using System.Windows.Input;
@@ -15,11 +16,45 @@ namespace NP.Demos.BuiltInControls
 #if DEBUG
             this.AttachDevTools();
 #endif
+
+            var openWindowButton = this.FindControl<Button>("OpenWindowButton");
+
+            openWindowButton.Click += OpenWindowButton_Click;
+
+            var openModalWindowButton = 
+                this.FindControl<Button>("OpenModalWindowButton");
+
+            openModalWindowButton.Click += OpenModalWindowButton_Click;
         }
 
-        private void Button_Click(object? sender, RoutedEventArgs e)
+        private void OpenWindowButton_Click(object? sender, RoutedEventArgs e)
         {
-            this.Close();
+            // Create the window object
+            Window sampleWindow = 
+                new Window 
+                { 
+                    Title = "Sample Window",
+                    Width = 200,
+                    Height = 200
+                };
+
+            // open the window
+            sampleWindow.Show();
+        }
+
+        private void OpenModalWindowButton_Click(object? sender, RoutedEventArgs e)
+        {
+            // Create the window object
+            Window sampleWindow = 
+                new Window 
+                { 
+                    Title = "Sample Modal (Dialog) Window",
+                    Width = 200,
+                    Height = 200
+                };
+
+            // open the window as modal dialog
+            sampleWindow.ShowDialog(this);
         }
 
         private void InitializeComponent()
