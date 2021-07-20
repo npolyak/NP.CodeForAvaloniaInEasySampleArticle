2979927106305d2e5e64863b02a1319cbce00767
npolyak <nick.polyak@verizon.net>
2021-07-20 19:14:44 -0400

    added Panels samples.

--- NP.Demos.Panels/MainWindow.axaml.cs
+++ NP.Demos.Panels/MainWindow.axaml.cs
@@ -1,30 +1,22 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Data.Converters;
 using Avalonia.Markup.Xaml;
-using Avalonia.Media;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform;
-using System;
 
-namespace NP.Demos.Primitives
+namespace NP.Demos.Panels
 {
     public partial class MainWindow : Window
     {
-
         public MainWindow()
         {
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
-
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
-
     }
 }
