3d6a42c976ee1d12e50d443b27b61ba2d83553aa
Philippe Laval <philippe.laval.1968@laposte.net>
2023-08-21 23:23:40 +0200

    Update the projects

---
+++ NP.Demos.Primitives/Converters/EnumTypeToCollectionConverter.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections;
+using System.Globalization;
+
+namespace NP.Demos.Primitives.Converters
+{
+    public class EnumTypeToCollectionConverter : IValueConverter
+    {
+        public static EnumTypeToCollectionConverter Instance { get; } =
+            new EnumTypeToCollectionConverter();
+
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            Type enumType = value as Type ?? targetType;
+
+            return new ArrayList(Enum.GetValues(enumType));
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
